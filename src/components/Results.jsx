5b680f508a26d296b6fb016b4bd32cead492bbfc
syrym-umbetov <umbetov.syrym@gmail.com>
2021-12-09 18:52:29 +0600

    images

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,8 +10,14 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults('/search/q=JavaScript Mastery&num=40');
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === '/videos') {
+        getResults(`/search/q=${searchTerm}videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   console.log(results);
 
@@ -22,13 +28,39 @@ export const Results = () => {
     case '/search':
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {/* {results?.results?.map(({ link, title }) => (
-
-          ))} */}
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className='md:w-2/5 w-full'>
+              <a href={link} target='_blank' rel='noreferrer'>
+                <p className='text-sm'>
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
         </div>
       );
     case '/images':
-      return 'SEARCH';
+      return (
+        <div className='flex flex-wrap justify-center items-center'>
+          {results?.image_results?.map(
+            ({ image, link: { href, title } }, index) => (
+              <a
+                className='sm:p-3 p-5'
+                href={href}
+                key={index}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <img src={image?.src} alt={title} loading='lazy' />
+                <p className='w-36 break-words text-sm mt-2'>{title}</p>
+              </a>
+            )
+          )}
+        </div>
+      );
     case '/news':
       return 'SEARCH';
     case '/videos':
